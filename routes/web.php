c212627fd1ef7d383f956eee353e5047c0d66483
unknown <putrichantiikaa@gmail.com>
2025-03-17 14:16:25 +0700

    membuat file beranda & file login

--- routes/web.php
+++ routes/web.php
@@ -25,46 +25,15 @@ Route::get('/', function () {
 //     return view('notfound');
 // });
 
-
-
-// Route::get('mahasiswa/{nama}', function ($nama) {
-//     echo "Ini Halaman Mahasiswa dengan Nama $nama";
-// });
-
-// Route::get('/hello/hello2/hellolagi', function () {
-//     echo "Hello World";
-// });
-
-// Route::get('/mahasiswa', function () {
-//     $kelas = "IS62";
-//     $data = ["Putri Chantika","Raihan Ramadhani","Nabiilah Aula Safda","Cut Putri Efrina","Sri Maharani","Penti Anggraini","Kania Nabila Muntaz"];
-
-//     // return view('mahasiswa.index')->with('mhs',$data)->with('kls',$kelas);
-//     return view('mahasiswa.index',compact('kelas','data'));
-// });
-
-// Route::get('/mahasiswa', function () {
-//     $nama = "Putri Chantika";
-//     $nilai = 90;
-
-//     $nilai2 = [40,70,36,99,100];
-
-//     return view('mahasiswa',compact('nama','nilai','nilai2'));
-// });
-
-Route::get('/mahasiswa', function () {
-    $data_mhs = ["Putri Chantika","Raihan Ramadhani","Nabiilah Aula Safda","Sri Maharani","Cut Putri Efrina"];
-    return view('data.mahasiswa',compact('data_mhs'));
+Route::get('/beranda', function () {
+    return view('welcome');
 });
 
-Route::get('/dosen', function () {
-    $data_dos = ["Ismanuddin","Mustofa Lutfi","Rita Warni","Ridha Ansari","Dzulgunar M Nasir"];
-    return view('data.dosen',compact('data_dos'));
-});
 
-Route::get('/galeri', function () {
-    return view('data.galeri');
-});
+
+
+
+
 
 
 
